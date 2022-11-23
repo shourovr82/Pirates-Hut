917e8b3ea243e64df6712a138507a32d1ad655bc
shourovr82 <shafinur512@gmail.com>
2022-11-23 22:47:56 +0600

    firebase added and signup page

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD1SXFcDM71Ocbo_3ldUb3AA28MLnm3DE",
+  authDomain: "air-cnc-8c2de.firebaseapp.com",
+  projectId: "air-cnc-8c2de",
+  storageBucket: "air-cnc-8c2de.appspot.com",
+  messagingSenderId: "320605445810",
+  appId: "1:320605445810:web:728aac73b6f87cd2def3ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app

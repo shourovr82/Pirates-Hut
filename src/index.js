9aaa658328b8c83a61da1541509265bb02534b9b
shourovr82 <shafinur512@gmail.com>
2022-11-23 23:54:18 +0600

    updated router provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import AuthProvider from './contexts/AuthProvider'
-import 'react-datepicker/dist/react-datepicker.css'
-import { Toaster } from 'react-hot-toast'
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import AuthProvider from './AuthContexts/Contexts/AuthProvider';
+import { Toaster } from 'react-hot-toast';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AuthProvider>
-    <Toaster />
-    <App />
-  </AuthProvider>
-)
+  <React.StrictMode>
+    <AuthProvider>
+      <Toaster />
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

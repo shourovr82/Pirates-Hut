f6dd43dcfe0030633817ae02bb67505bdc28a276
shourovr82 <shafinur512@gmail.com>
2022-11-23 23:50:51 +0600

    router provider changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/routes'
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <div>
+
+    </div>
+  )
 }
 
 export default App
